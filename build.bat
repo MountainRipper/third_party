47a95abd66959723cc9725081cca645b13cb4421
xuwei <xuwei17385@163.com>
2023-03-06 13:57:32 +0800

    add imgui helper

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
-set http_proxy=http://127.0.0.1:1080
-set https_proxy=http://127.0.0.1:1080
+::set http_proxy=http://127.0.0.1:1080
+::set https_proxy=http://127.0.0.1:1080
 set DIR_CUR=%~dp0
 set DIR_TOOLS=%DIR_CUR%tools
 SET INSTALLER_MSYS2=%DIR_TOOLS%\msys2-x86_64-20230127.exe
@@ -9,8 +9,8 @@ SET URL_MSYS2="https://github.com/msys2/msys2-installer/releases/download/2023-0
 if exist %INSTALLER_MSYS2% (
 	echo %INSTALLER_MSYS2% is exist!
 ) else (
-	echo *****%INSTALLER_MSYS2% is not exist! downloading from %URL_MSYS2%
-	echo *****You can also download and put it in tools folder.
+	echo ***** %INSTALLER_MSYS2% is not exist! downloading from %URL_MSYS2%
+	echo ***** You can also download and put it in tools folder.
 	cd %DIR_TOOLS%
 	wget %URL_MSYS2%
 )
@@ -18,17 +18,15 @@ if exist %INSTALLER_MSYS2% (
 if exist %DIR_MSYS2% (
 	echo %DIR_MSYS2% is exist!
 ) else (
-	echo *****INSTALL MSYS2 TO %DIR_MSYS2%
+	echo ***** INSTALL MSYS2 TO %DIR_MSYS2%
 	%INSTALLER_MSYS2% in --root %DIR_MSYS2% --accept-messages  --accept-licenses --confirm-command
-	cp %DIR_MSYS2%\etc\fstab %DIR_MSYS2%\etc\fstab.bak
-	echo $DIR_CUR /projects/MountainRipper/third_party  >> %DIR_MSYS2%\etc\fstab
+	copy %DIR_MSYS2%\etc\fstab %DIR_MSYS2%\etc\fstab.bak
+	echo %DIR_CUR% /projects/MountainRipper/third_party  >> %DIR_MSYS2%\etc\fstab
 )
 
-copy %DIR_MSYS2%\etc\fstab %DIR_MSYS2%\etc\fstab.bak
-echo %DIR_CUR% /projects/MountainRipper/third_party  >> %DIR_MSYS2%\etc\fstab
 
-echo ******Now open a msys2 shell to build libraries
-echo ******$ cd /projects/third_party
-echo ******$ ./build.sh
+echo ****** Now open a msys2 shell to build libraries
+echo ****** $ cd /projects/MountainRipper/third_party
+echo ****** $ ./build.sh
 start %DIR_MSYS2%\mingw64.exe
 cd 
