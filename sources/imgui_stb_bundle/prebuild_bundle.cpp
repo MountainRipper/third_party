51ed71d5d832744578c128901f0f15bcfdee4b1a
xuwei <xuwei17385@153.com>
2023-07-20 22:25:27 +0800

    update imgui to 1.89.7, drop mrbundle instead of imgui_stb

---
+++ sources/imgui_stb_bundle/prebuild_bundle.cpp
@@ -0,0 +1,21 @@
+#define IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include <glad/gl.h>
+#include <imgui/backends/imgui_impl_opengl3.cpp>
+#include <imgui/backends/imgui_impl_sdl2.cpp>
+
+#if defined(WIN32) || defined(WIN64)
+#include <imgui/backends/imgui_impl_win32.cpp>
+#endif
+
+#define IMGUI_IMPLEMENTATION
+//#define IMGUI_ENABLE_FREETYPE
+#include <imgui/misc/single_file/imgui_single_file.h>
+
+#include <imgui/misc/cpp/imgui_stdlib.cpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb/stb_image_write.h>
